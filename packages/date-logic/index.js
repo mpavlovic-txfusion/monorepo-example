1aabaefd8f6b169d45b89ec03e144526a6feea87
mpavlovic-txfusion <mpavlovic@txfusion.io>
2023-04-04 17:13:45 +0200

    Change date format

--- packages/date-logic/index.js
+++ packages/date-logic/index.js
@@ -1,6 +1,6 @@
 module.exports = function getDate(
     locale = 'en-US',
-    options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
+    options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' }
 ) {
     return new Date().toLocaleDateString(locale, options);
 }
