a480aff52663fb1d05efaf52e535f0e151fc162c
mpavlovic-txfusion <mpavlovic@txfusion.io>
2023-04-09 11:37:32 +0200

    Improve create release script

--- scripts/create-release-from-tags.js
+++ scripts/create-release-from-tags.js
@@ -1,3 +1,4 @@
+import { Command } from 'commander';
 import spawn from '@npmcli/promise-spawn';
 import getPackages from 'get-monorepo-packages';
 import path from 'path';
@@ -5,62 +6,46 @@ import fs from 'fs';
 
 /**
  *
- * @returns list of tags
+ * @returns list of tags based on package name
  * @example ["@mpavlovic-txfusion/date-logic@2.2.2", "@mpavlovic-txfusion/date-logic@2.2.2"]
  */
-const getCurrentGitTags = async () => {
+const getCurrentGitTagsForPackage = async (packageName) => {
   const { stdout, stderr, code } = await spawn('git', [
     'tag',
     '--points-at',
     'HEAD',
     '--column',
-  ])
+  ]);
+  
   if (code !== 0) {
-    throw new Error(stderr.toString())
+    throw new Error(stderr.toString());
   }
 
-  return parseRawTags(stdout.toString())
-}
-
-const getConfig = async ({
-  DRY_RUN,
-  TAGS,
-}) => {
-  const isDryRun = Boolean(DRY_RUN);
-  const tags = TAGS ? parseRawTags(TAGS) : await getCurrentGitTags();
-
-  if (!tags.length) {
-    throw new Error('No git tags found.');
-  }
-  return {
-    isDryRun,
-    tags,
-  };
-}
+  return parseRawTags(stdout.toString(), packageName);
+};
 
 const getChangelogPath = (packageName)=> {
-  const result = getPackages('.').find((p) =>
-    p.package.name.includes(packageName)
-  );
-  if (!result)
+  const result = getPackages('.').find((p) => p.package.name.includes(packageName));
+  if (!result) {
     throw new Error(`could not find package with name: ${packageName}.`);
+  }
 
   let changelogPath;
   for (const fileName of ['CHANGELOG.MD', 'CHANGELOG.md']) {
-    if (changelogPath) break;
     const myPath = path.join(result.location, fileName);
     const pathExists = fs.existsSync(myPath);
     if (pathExists) {
       changelogPath = myPath;
+      break;
     }
   }
 
-  if (changelogPath) {
-    return changelogPath;
-  } else {
+  if (!changelogPath) {
     console.log(`could not find changelog path for ${result.location}`);
   }
-}
+
+  return changelogPath;
+};
 
 /**
  *
@@ -76,39 +61,49 @@ const createGithubRelease = async (tag, releaseNotes) => {
     tag,
     '--notes',
     releaseNotes || '',
-  ])
+  ]);
+
   if (code !== 0) {
-    throw new Error(stderr.toString())
+    throw new Error(stderr.toString());
   }
-}
+};
 
 /**
  *
- * @param rawTag - ex. "@segment/analytics-foo@1.99.0"
+ * @param rawTag - ex. "@mpavlovic-txfusion/date-logic@2.2.2"
  */
-const extractPartsFromTag = (rawTag)=> {
-  const [name, version] = rawTag.split(/@(\d.*)/)
-  if (!name || !version) return undefined
+const extractPartsFromTag = (rawTag) => {
+  const [name, version] = rawTag.split(/@(\d.*)/);
+  if (!name || !version) {
+    return undefined;
+  }
+
   return {
     name,
     versionNumber: version?.replace('\n', ''),
     raw: rawTag,
-  }
-}
+  };
+};
+
+/**
+ * Removes nullish values.
+ */
+const exists = (value) => {
+  return value != null && value !== undefined;
+};
 
 /**
  *
  * @param rawTags - string delimited list of tags (e.g. `@mpavlovic-txfusion/date-logic@2.2.2 @mpavlovic-txfusion/date-renderer@1.2.0`)
  */
-const parseRawTags = (rawTags) => {
-  return rawTags.trim().split(' ').map(extractPartsFromTag).filter(exists);
-}
+const parseRawTags = (rawTags, packageName) => {
+  let tags = rawTags.trim().split(' ').map(extractPartsFromTag).filter(exists);
 
-/**
- * Type guard for removing nullish values.
- */
-const exists = (value) => {
-  return value != null && value !== undefined;
+  if (packageName) {
+    tags = tags.filter((tag) => tag.name.includes(packageName));
+  }
+
+  return tags;
 };
 
 /**
@@ -124,7 +119,9 @@ const parseReleaseNotes = (
   let end;
 
   changelogText.split('\n').forEach((line, idx) => {
-    if (begin && end) return;
+    if (begin && end) {
+      return;
+    }
     if (line.includes(versionNumber)) {
       begin = idx + 1;
     } else if (begin && h2tag.test(line)) {
@@ -134,9 +131,10 @@ const parseReleaseNotes = (
 
   const result = changelogText.split('\n').filter((_, idx) => {
     return idx >= begin && idx <= (end ?? Infinity);
-  })
+  });
+  
   return result.join('\n');
-}
+};
 
 const getReleaseNotes = (tag) => {
   const { name, versionNumber } = tag;
@@ -153,43 +151,44 @@ const getReleaseNotes = (tag) => {
     );
   };
   return releaseNotes;
-}
+};
 
-const createGithubReleaseFromTag = async (
-  tag,
-  { dryRun = false } = {}
-) => {
-  const notes = getReleaseNotes(tag)
+const createGithubReleaseFromTag = async (tag) => {
+  const notes = getReleaseNotes(tag);
   if (notes) {
     console.log(
       `\n ---> Outputting release titled: ${tag.raw} with notes: \n ${notes}`
     )
   }
 
-  if (dryRun) {
-    console.log(`--> Dry run: ${tag.raw} not released.`);
-    return;
-  }
-
   await createGithubRelease(tag.raw, notes);
-}
+};
 
-const createReleaseFromTags = async (config) => {
-  console.log('Processing tags:', config.tags, '\n');
+const createReleaseFromTags = async (tags) => {
+  console.log('Processing tags: ', tags, '\n');
 
-  for (const tag of config.tags) {
-    await createGithubReleaseFromTag(tag, { dryRun: config.isDryRun });
+  for (const tag of tags) {
+    await createGithubReleaseFromTag(tag);
   }
+};
+
+async function main() {
+  const program = new Command();
+
+  program
+    .name('create-github-release-from-tags')
+    .option('-p, --package', 'Package name to create release for');
+
+  // TODO: Remove this
+  console.log('PACKAGE NAME: ', program.opts().package);
+
+  const tags = await getCurrentGitTagsForPackage(program.opts().package);
+  return createReleaseFromTags(tags);
 }
 
-async function run() {
-  const config = await getConfig(process.env)
-  return createReleaseFromTags(config)
-}
-
-run()
-    .then(() => process.exit(0))
-    .catch((err) => {
-        console.error('Error:', err);
-        process.exit(1);
-    });
+main()
+  .then(() => process.exit(0))
+  .catch((err) => {
+    console.error('Error:', err);
+    process.exit(1);
+  });
